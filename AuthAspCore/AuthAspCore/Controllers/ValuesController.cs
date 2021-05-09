404e3dd21f4459459268144596f8188be1802c29
Pruthvi Kalidindi <Pruthvi.Kalidindi@infor.com>
2021-05-09 10:59:54 +0530

    Init working copy of Authorization and Authentication on .NET Core WebApi

---
+++ AuthAspCore/AuthAspCore/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthAspCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "Auth Web API Core 5.0";
+        }
+    }
+}
